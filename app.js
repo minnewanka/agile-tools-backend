30beb92725031ebd618901cec921c1d6aa5fe805
Philippe INTHAVONG <philippe.inthavong@SIICANADA.COM>
2019-02-28 17:25:12 -0500

    fix app.js

--- app.js
+++ app.js
@@ -14,25 +14,21 @@ var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI
 
 // Import parse 
 var api = new ParseServer({
-    databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
-    cloud: process.env.CLOUD_CODE_MAIN || './cloud/main.js',
-    appId: process.env.APP_ID || 'SIIAG',
-    masterKey: process.env.MASTER_KEY || 'qWapk8BYDbUdnfMl3hZ8b2yTAgglDQ', 
-    liveQuery: {
-      classNames: ['Vote']
-    }
-  })
+  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+  appId: process.env.APP_ID || 'SIIAG',
+  masterKey: process.env.MASTER_KEY || 'qWapk8BYDbUdnfMl3hZ8b2yTAgglDQ',  //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  liveQuery: {
+    classNames: ["Vote"] // List of classes to support for query subscriptions
+  }
+})
 
 //don't show the log when it is test
 app.use(logger("dev"))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(cookieParser())
 app.use(express.static(path.join(__dirname, "public")))
 //SwaggerUI
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
-
 app.use("/", indexRouter)
 
 // Serve the Parse API on the /parse URL prefix
@@ -41,14 +37,13 @@ app.use(mountPath, api)
 
 //app.listen(1337, () => {})
 
-var port = process.env.PORT || 1337
-var httpServer = require('http').createServer(app)
-
+var port = process.env.PORT || 1337;
+var httpServer = require('http').createServer(app);
 httpServer.listen(port, function() {
-    console.log('parse-server-example running on port ' + port + '.')
+    console.log('parse-server-example running on port ' + port + '.');
 })
 
 // This will enable the Live Query real-time server
-ParseServer.createLiveQueryServer(httpServer)
+ParseServer.createLiveQueryServer(httpServer);
 
 module.exports = app
