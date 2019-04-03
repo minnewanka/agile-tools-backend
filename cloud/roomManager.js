06e82aa9dd5afb7863c813e04bd8230c7213fd7f
minnewanka <philippe.inthavong@gmail.com>
2019-04-03 16:11:27 -0400

    check if room exist

--- cloud/roomManager.js
+++ cloud/roomManager.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-undef*/ // no need to import Parse in cloud functions
-var Room = Parse.Object.extend("Room")
+var Room = Parse.Object.extend('Room')
 
 var generateCodeNumber = () => {
   var CODE_NUMBER_LENGTH = 5
-  var codeNumber = ""
-  var possible = "123456789"
+  var codeNumber = ''
+  var possible = '123456789'
   for (var i = 0; i < CODE_NUMBER_LENGTH; i++) {
     codeNumber += possible.charAt(Math.floor(Math.random() * possible.length))
   }
@@ -14,12 +14,12 @@ var generateCodeNumber = () => {
 /**
  *   Generate an unique code when a new Room is being created
  */
-Parse.Cloud.beforeSave("Room", async request => {
+Parse.Cloud.beforeSave('Room', async request => {
   var room = request.object
   const maxLengthName = 25
-  if (room.get("name").length > maxLengthName) {
+  if (room.get('name').length > maxLengthName) {
     return Promise.reject({
-      code: "ERR-004",
+      code: 'ERR-004',
       message: `Room name length must be less than ${maxLengthName} characters long`
     })
   }
@@ -30,23 +30,20 @@ Parse.Cloud.beforeSave("Room", async request => {
     trialCount++
     var newCodeNumber = generateCodeNumber()
     var query = new Parse.Query(Room)
-    var existingOrder = await query
-      .equalTo("code", newCodeNumber)
-      .first()
-      .then(existingOrder => {
-        if (existingOrder) {
-          if (trialCount >= MAX_TRIAL_COUNT) {
-            throw { code: 1011, message: "A server error occurred." }
-          } else {
-            //code number duplicated, retrying...
-            tryCreateNewCodeNumber()
-          }
-        } else {
-          //set code room ..")
-          var room = request.object
-          room.set("code", newCodeNumber)
-        }
-      })
+    var existingOrder = await query.equalTo('code', newCodeNumber).first()
+
+    if (existingOrder) {
+      if (trialCount >= MAX_TRIAL_COUNT) {
+        throw { code: 1011, message: 'A server error occurred.' }
+      } else {
+        //code number duplicated, retrying...
+        tryCreateNewCodeNumber()
+      }
+    } else {
+      //set code room ..")
+      var room = request.object
+      room.set('code', newCodeNumber)
+    }
   }
   await tryCreateNewCodeNumber()
 })
--- cloud/voteManager.js
+++ cloud/voteManager.js
@@ -1,57 +1,66 @@
 /* eslint-disable no-undef*/ // no need to import Parse in cloud functions
-var Vote = Parse.Object.extend("Vote")
+var Vote = Parse.Object.extend('Vote')
 
-Parse.Cloud.beforeSave("Vote", async request => {
+Parse.Cloud.beforeSave('Vote', async request => {
   var vote = request.object
 
   const maxLengthName = 20
-  if (vote.get("username") && vote.get("username").length > maxLengthName) {
+  if (vote.get('username') && vote.get('username').length > maxLengthName) {
     return Promise.reject({
-      code: "ERR-003",
+      code: 'ERR-003',
       message: `Participant username length must be less than ${maxLengthName} characters long`
     })
+  }
+
+  var checkIfRoomExist = async () => {
+    var Room = Parse.Object.extend('Room')
+    var query = new Parse.Query(Room)
+    var room = await query.equalTo('code', vote.get('roomCode')).first()
+    if (!room) {
+      return Promise.reject({
+        code: 'ERR-005',
+        message: `Room with code ${vote.get('roomCode')} doesn't exist`
+      })
+    }
   }
 
   var checkIfAlreadyExist = async () => {
     var query = new Parse.Query(Vote)
     var existingVote = await query
-      .equalTo("username", vote.get("username"))
-      .equalTo("roomCode", vote.get("roomCode"))
+      .equalTo('username', vote.get('username'))
+      .equalTo('roomCode', vote.get('roomCode'))
       .first()
-      .then(existingVote => {
-        if (
-          existingVote &&
-          vote.get("username") === existingVote.get("username") &&
-          vote.id !== existingVote.id
-        ) {
-          return Promise.reject({
-            code: "ERR-001",
-            message: `Participant with name ${vote.get(
-              "username"
-            )} already exist`
-          })
-        }
+    if (
+      existingVote &&
+      vote.get('username') === existingVote.get('username') &&
+      vote.id !== existingVote.id
+    ) {
+      return Promise.reject({
+        code: 'ERR-001',
+        message: `Participant with name ${vote.get('username')} already exist`
       })
+    }
   }
 
   var checkifAvailableSpace = async () => {
     const maxParticipants = 20
     var query = new Parse.Query(Vote)
     const participants = await query
-      .equalTo("roomCode", vote.get("roomCode"))
+      .equalTo('roomCode', vote.get('roomCode'))
       .find()
     if (
       !participants.map(participant => participant.id).includes(vote.id) &&
       participants.length >= maxParticipants
     ) {
       return Promise.reject({
-        code: "ERR-002",
+        code: 'ERR-002',
         message: `Room with code ${vote.get(
-          "roomCode"
+          'roomCode'
         )} is full (max ${maxParticipants} participants)`
       })
     }
   }
+  await checkIfRoomExist()
   await checkifAvailableSpace()
   await checkIfAlreadyExist()
 })
