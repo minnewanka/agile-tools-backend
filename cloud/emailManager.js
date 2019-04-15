d34b7b0a67409854402a7c41df81559b2f99083d
Philippe INTHAVONG <philippe.inthavong@SIICANADA.COM>
2019-04-15 13:11:34 -0400

    config mail for prod

--- cloud/emailManager.js
+++ cloud/emailManager.js
@@ -1,25 +1,38 @@
-const sendmail = require("sendmail")({
-  devPort: 25, // Default: False
-  devHost: "localhost", // Default: localhost
-  smtpPort: 25, // Default: 25
-  smtpHost: "localhost" // Default: -1 - extra smtp host after resolveMX
+"use strict"
+const nodemailer = require("nodemailer")
+
+let transporter = nodemailer.createTransport({
+  host: "smtp.office365.com",
+  secureConnection: false,
+  port: 587,
+  tls: {
+    ciphers: 'SSLv3'
+  },
+  auth: {
+    user: "agile-tools@siicanada.com",
+    pass: "@SIIcanada1"
+  }
 })
+
+
 // no need to import Parse in cloud functions
 /* eslint-disable no-undef*/
 Parse.Cloud.define("sendFeedback", async request => {
   const {
-    params: { firstname, lastname, email, company, message, subject }
+    params: {
+      firstname,
+      lastname,
+      email,
+      company,
+      message,
+      subject
+    }
   } = request
   const content = `First Name: ${firstname}<br/>Last Name: ${lastname}<br/>Email: ${email}<br/>Company: ${company}<br/>Message: ${message}`
-  sendmail(
-    {
-      from: "agileapp@siicanada.com",
-      to: "agileapp@siicanada.com",
-      subject: `[${subject}] Agile Tools`,
-      html: content
-    },
-    function(err, reply) {
-      console.log(err && err.stack)
-    }
-  )
+  transporter.sendMail({
+    from: "agile-tools@siicanada.com",
+    to: "agile-tools@siicanada.com",
+    subject: `[${subject}] Agile Tools`,
+    html: content
+  })
 })
