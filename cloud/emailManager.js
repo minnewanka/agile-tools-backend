e5cbeb7f331d1ca713e4cf1604f60d963ebf9a3e
Philippe INTHAVONG <philippe.inthavong@SIICANADA.COM>
2019-04-18 14:17:25 -0400

    parametrize email from and to

--- cloud/emailManager.js
+++ cloud/emailManager.js
@@ -1,37 +1,29 @@
-"use strict"
-const nodemailer = require("nodemailer")
+'use strict'
+const nodemailer = require('nodemailer')
 
 let transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST || "localhost",
+  host: process.env.SMTP_HOST || 'localhost',
   secureConnection: false,
   port: process.env.SMTP_PORT || 25,
   tls: {
     ciphers: 'SSLv3'
   },
   auth: {
-    user: process.env.SMTP_USER || "user",
-    pass: process.env.SMTP_PASSWORD || "password"
+    user: process.env.SMTP_USER || 'user',
+    pass: process.env.SMTP_PASSWORD || 'password'
   }
 })
 
-
 // no need to import Parse in cloud functions
 /* eslint-disable no-undef*/
-Parse.Cloud.define("sendFeedback", async request => {
+Parse.Cloud.define('sendFeedback', async request => {
   const {
-    params: {
-      firstname,
-      lastname,
-      email,
-      company,
-      message,
-      subject
-    }
+    params: { firstname, lastname, email, company, message, subject }
   } = request
   const content = `First Name: ${firstname}<br/>Last Name: ${lastname}<br/>Email: ${email}<br/>Company: ${company}<br/>Message: ${message}`
   transporter.sendMail({
-    from: "agile-tools@siicanada.com",
-    to: "agile-tools@siicanada.com",
+    from: process.env.SMTP_USER,
+    to: process.env.SMTP_USER,
     subject: `[${subject}] Agile Tools`,
     html: content
   })
